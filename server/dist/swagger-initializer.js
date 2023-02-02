dd4932a38979fcfe7429002049d7311aabb3197b
unknown <satrgomez7@gmail.com>
2023-02-02 10:37:15 -0400

    Updated swagger-initializer.js

--- server/dist/swagger-initializer.js
+++ server/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:1234/swagger.json",
+    url: "https://johns-bookstore-server.herokuapp.com/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
